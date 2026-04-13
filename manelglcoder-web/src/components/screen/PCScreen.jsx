9d67ad785df91e82f6d0beec7b59cba820856616
ManelGLCoder <manelglcoder@gmail.com>
2026-04-13 17:53:52 +0200

    little url fix

--- manelglcoder-web/src/components/screen/PCScreen.jsx
+++ manelglcoder-web/src/components/screen/PCScreen.jsx
@@ -6,7 +6,7 @@ const PCScreen = () =>{
     return(
         <>
             <section className={`flex flex-col min-h-svh min-w-svw 
-            bg-desktop bg-[url('/public/ManelGLCoder.svg')] bg-no-repeat bg-center bg-contain`}>
+            bg-desktop bg-[url('/ManelGLCoder.svg')] bg-no-repeat bg-center bg-contain`}>
                 <TopBarDesktop/>
                 <Desktop/>
             </section>
