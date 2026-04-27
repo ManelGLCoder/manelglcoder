2eab63078996686d067eac5de4898a884dc584fc
ManelGLCoder <manelglcoder@gmail.com>
2026-04-27 14:27:53 +0200

    add icons in topbar

--- manelglcoder-web/src/components/TopBars/TopBarDesktop.jsx
+++ manelglcoder-web/src/components/TopBars/TopBarDesktop.jsx
@@ -1,4 +1,8 @@
 import { useState, useEffect } from "react"
+
+import ClockIcon from '../../assets/Icons/clock_icon.svg'
+import DateIcon from '../../assets/Icons/date_icon.svg'
+
 const TopBarDesktop = () =>{
 
     const [resetInterval,setResetInterval] = useState(false)
@@ -30,8 +34,10 @@ const TopBarDesktop = () =>{
         <section className={`flex min-w-svw max-h-10 px-5
         justify-between items-center text-red-dark-logo text-2xl bg-topbar`}>
             <span className="mx-2 font-logo">ManelGLCoder</span>
-            <div className="flex gap-5 font-pixel-number">
-                <span >{currDate}</span>
+            <div className="flex gap-2 justify-center items-center font-pixel-number">
+                <img className="size-7" src={DateIcon} alt="Date Icon" />
+                <span>{currDate}</span>
+                <img className="size-7" src={ClockIcon} alt="Clock Icon" />
                 <span>{currTime}</span>
             </div>
         </section>
